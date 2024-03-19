c8bfc85e3918ad75fb78d126ed037b2e024e1a22
VuCuongHD96 <xuancuonghd1102@gmail.com>
2024-03-19 09:57:36 +0700

    Initial Commit

---
+++ CPlusFuncExample/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPlusFuncExample
+//
+//  Created by Work on 19/03/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
